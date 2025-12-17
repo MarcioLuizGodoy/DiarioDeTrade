72e89b79f13edf1688292c79c1b8350086beb365
MarcioLuizGodoy <marciooliveirasilvagodoy@gmail.com>
2025-12-17 02:00:15 -0300

    pequenas mudanças

---
+++ dist-exe/dbCode.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE DiarioDeTrade;
+
+USE DiarioDeTrade;
+
+CREATE TABLE Usuarios (
+    nome VARCHAR(200),
+    senha VARCHAR(200)
+);
+CREATE TABLE Operacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_ativo VARCHAR(200) NOT NULL,
+    ativo VARCHAR(200) NOT NULL,
+    preco_entrada BIGINT NOT NULL,
+    preco_saida BIGINT NOT NULL,
+    quantidade_contratos INT NOT NULL,
+    tipo_operacao VARCHAR(200) NOT NULL,
+    tipo_posicao VARCHAR(100) NOT NULL,
+    status_operacao VARCHAR(150) NOT NULL,
+    imagem LONGBLOB,
+    data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+    descricao TEXT,
+    evento_tecnico_base VARCHAR (100)
+);
+
+
+
+INSERT INTO Usuarios (nome, senha) VALUES ('user','user');
+-- INSERT INTO Usuarios (nome, senha) VALUES ('godoy', 'godoy');
+
+
+
+
